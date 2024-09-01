ec106f75718216d2b2e0e08abd2ed2e25565d74c
sulbum yun <sulbumyun@sulbumui-MacBookPro.local>
2024-09-01 19:19:38 +0900

    mustreadtitle

--- src/Ihavenobrain.jsx
+++ src/Ihavenobrain.jsx
@@ -4,7 +4,46 @@ import "./Ihavenobrain.css";
 export default function Ihavenobrain() {
   return (
     <div>
-      <div className="Header">헤더어</div>
+      <div className="Header">
+        <div className="Menu">메뉴1</div>
+        <div className="Menu">메뉴2</div>
+        <div className="Menu">메뉴3</div>
+        <div className="Menu">메뉴4</div>
+        <div className="Menu">메뉴5</div>
+      </div>
+      <div className="body">
+        <h1>보지 아니하지 아니할 수 없는 대자보</h1>
+        <div class="scroll">
+          <div>
+            <span>
+              보아라 사랑이 고픈 소년들아 보아라 사랑을 나눌 소녀들아 청춘의
+              환상과 정열 가득 모아 정글은 언제나 맑음 뒤 흐림!
+            </span>
+            <span>
+              보아라 사랑이 고픈 소년들아 보아라 사랑을 나눌 소녀들아 청춘의
+              환상과 정열 가득 모아 정글은 언제나 맑음 뒤 흐림!
+            </span>
+          </div>
+        </div>
+        <div class="topdown">
+          <div>
+            <span>
+              보아라 사랑이 고픈 소년들아 보아라 사랑을 나눌 소녀들아 청춘의
+              환상과 정열 가득 모아 정글은 언제나 맑음 뒤 흐림!
+            </span>
+            <span>
+              보아라 사랑이 고픈 소년들아 보아라 사랑을 나눌 소녀들아 청춘의
+              환상과 정열 가득 모아 정글은 언제나 맑음 뒤 흐림!
+            </span>
+          </div>
+        </div>
+      </div>
+      {/* <div className="orgin">
+        dma
+        <div className="first">처음</div>
+        <div className="second">둘둘</div>
+      </div> */}
+
       <div className="Footer">
         <div class="Footer-content">여기에 텍스트가 나타납니다.</div>
       </div>
